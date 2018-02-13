03f5fbfc330d7378f7af895fb4e86e89c16a09dd
Singer  Daniel <dsinger@ethz.ch>
2018-02-13 14:59:41 +0100

    Emgu CV is Integrated
    .Loading via binding to Source Path is supported anyways
    .BitMapConverter as a static class to convert Emgu Image<> to BitMapSource, which can be bound to from the XAML

---
+++ WpfVirginEmgu/WpfVirginEmgu/Model/ImageContainer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Emgu.CV;
+using Emgu.CV.Structure;
+
+namespace WpfVirginEmgu.Model
+{
+    public class ImageContainer
+    {
+        private Image<Bgr, Byte> image;
+
+        public Image<Bgr, Byte> Image
+        {
+            get => image;
+            set => image = value;
+        }
+
+        public void LoadImage(string filePath)
+        {
+
+        }
+    }
+}
---
+++ WpfVirginEmgu/WpfVirginEmgu/ViewModel/ImageViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Windows.Input;
+using System.Drawing;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using Microsoft.Win32;
+using WpfVirginEmgu.Model;
+using WpfVirginEmgu.ViewModels;
+
+namespace WpfVirginEmgu.ViewModel
+{
+    public class ImageViewModel : BaseViewModel
+    {
+        public string ImageSource { get; set; }
+
+        public BitmapSource Image { get; set; }
+        
+        public ICommand LoadCommand => new RelayCommand(LoadImage, true);
+
+        private void LoadImage()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageSource = openFileDialog.FileName;
+                Image = BitMapConverter.ToBitmapSource(new Image<Bgr, Byte>(ImageSource));
+            }
+        }
+    }
+}
